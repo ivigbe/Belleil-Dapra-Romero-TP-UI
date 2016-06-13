2018f137529a1a2c5718fda96f5bb8716b246730
ivigbe <ivanbelleil@gmail.com>
2016-06-13 17:17:28 -0300

    Agregado proyecto android

---
+++ GatoEncerradoMobile/app/src/main/java/ar/edu/unq/programacionui/gatoencerradomobile/MainLaberintos.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.programacionui.gatoencerradomobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLaberintos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_laberintos);
+    }
+}
